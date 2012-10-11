e25ca2089c0b43cadd4b812d9d188f4d91c956a6
Tim Conkling <tconkling@gmail.com>
2012-10-10 17:37:57 -0700

    more work on generation

--- genpages/generator.py
+++ genpages/generator.py
@@ -1,26 +1,73 @@
 #
 # microtome - Tim Conkling, 2012
 
+import pystache
 from spec import *
 
-class Generator:
+class AttrWrapper(object):
+    def __init__ (self, delegate):
+        self._delegate = delegate
+
+    def __getattr__ (self, name):
+        return getattr(self._delegate, name)
+
+class PropView(AttrWrapper):
+    def __init__ (self, prop):
+        AttrWrapper.__init__(self, prop)
+        self.prop = prop
+
+    def type (self):
+        type = self.prop.type
+        if type == "bool":
+            return "BOOL"
+        else: return type
+
+
+class PageView(AttrWrapper):
     def __init__ (self, page):
-        self._page = page
+        AttrWrapper.__init__(self, page)
+        self.page = page
+
+    def props (self):
+        return [PropView(prop) for prop in self.page.props]
+
+    def superclass (self): return page.superclass or "MTMutablePage"
+
+
+class Generator(object):
+    def __init__ (self, page):
+        self._view = PageView(page)
 
     def generate (self):
         return ""
 
-    @property
-    def int_name (self): return "int"
+class ObjCGenerator(Generator):
+    def generate (self):
+        stache = pystache.Renderer()
+        return stache.render(OBJC_HEADER_TEMPLATE, self._view)
 
-    @property
-    def float_name (self): return "float"
+OBJC_HEADER_TEMPLATE = '''
+{{header}}
 
-    @property
-    def bool_name (self): return "bool"
+@interface {{name}} : {{superclass}}
+
+{{#props}}
+@property (nonatomic,readonly) {{type}} {{name}};
+{{/props}}
+
+@end
+'''
 
 if __name__ == "__main__":
-    page = Page(name = "test", superclass = None, props = [], pos = 0)
-    generator = Generator(page)
+
+    page = Page(name = "TestPage",
+        superclass = None,
+        props = [
+            Prop(type = "bool", subtype = None, name = "foo", attrs = None, pos = 0),
+            Prop(type = "PageRef", subtype = "MyPage", name = "bar", attrs = None, pos = 0)
+        ],
+        pos = 0)
+
+    generator = ObjCGenerator(page)
     out = generator.generate()
     print out
